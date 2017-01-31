745a0098eeb40d113501d4b4e2f5c4de8711a128
lucky <malinpavel@gmail.com>
2017-01-31 02:06:28 +0200

    qsort implementation without recursion.

--- qsort.nim
+++ qsort.nim
@@ -1,19 +1,64 @@
-import lists
-import random
+import lists,
+       random,
+       math
 
-proc generate_unsorted_list[T](count: int) : SinglyLinkedList[T] =
-  var unsorted_list = initSinglyLinkedList[T]()
+proc fillUnsortedList(unsortedItems : var seq[int]) =
   var i = 0
-  while i < count:
-    var random_item = random(1000)
-    unsorted_list.prepend(random_item)
+  while i < unsortedItems.len :
+    unsortedItems[i] = random(10)
     inc(i)
-  unsorted_list
+
+proc getMediana(unsortedItems : seq[int], l, r : int) : int = 
+  var min, max = unsortedItems[l]
+  var i = l
+  while i < r :
+    var item = unsortedItems[i]
+    if item < min : 
+      min = item
+    else:
+      if item > max :
+        max = item
+    inc(i)
+  ((max + min) / 2).int
+
+proc quicksort(unsortedItems : var seq[int]) =
+  var operations = initDoublyLinkedList[tuple[l : int, r : int]]()
+  prepend(operations, (l : 0, r : unsortedItems.high))
+
+  while operations.head != nil :
+    var operation = operations.head.value
+    remove(operations, operations.head)
+    
+    var left = operation.l
+    var right = if operation.r >= 0 : operation.r else : unsortedItems.high
+
+    if (right - left + 1) < 2  :
+      continue
+
+    var mediana = getMediana(unsortedItems, left, right)
+
+    while left <= right :
+      if unsortedItems[left] < mediana :
+        inc(left)
+        continue
+
+      if unsortedItems[right] > mediana : 
+        dec(right)
+        continue
+
+      if left <= right:
+        swap(unsortedItems[left], unsortedItems[right])
+        inc(left)
+        dec(right)
+      
+    prepend(operations, (l : operation.l, r : right))
+    prepend(operations, (l : left, r : operation.r))          
 
 proc main() =
-  var items: SinglyLinkedList[int]
-  items = generate_unsorted_list[int](100)
-  echo("unsorted collection : ", items)
+  var unsortedItems = newSeq[int](100)
+  fillUnsortedList(unsortedItems)
+  echo unsortedItems
+  quicksort(unsortedItems)
+  echo unsortedItems
 
 main()
-
